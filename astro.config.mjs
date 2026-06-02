d2195a889e95a1f136693ea348b0e1e3f112077e
Zach Metteer <drpsyber@yahoo.com>
2026-06-02 11:07:02 -0500

    feat: switch adapter from Vercel to Node and update start script path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: node({ output: 'server' }),
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
